7ebf30c6b0a888df3fc092cf457e27e323798940
Shirsath <c-sushant.shirsath@charter.com>
2026-05-20 02:33:36 +0530

    deploy-to-ec2

--- mongoC.js
+++ mongoC.js
@@ -1,13 +1,14 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+const connectionString = `mongodb+srv://sushantvilasshirsath_db_user:${password}@devcluster.lyhzsgg.mongodb.net/?appName=DevCluster&retryWrites=true&w=majority`; // clustore url
+// mongodb+srv://sushantvilasshirsath_db_user:<db_password>@devcluster.lyhzsgg.mongodb.net/?appName=DevCluster
 const client = new MongoClient(connectionString);
 let conn;
 try {
   conn = await client.connect();
-  console.log("connection successful")
-} catch(e) {
+  console.log("connection successful");
+} catch (e) {
   console.error(e);
 }
 let db = conn.db("integration_ninjas");
